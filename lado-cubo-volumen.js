365a65078a01f260408b3d4c7c2661d605de1eac
Mauriciom20 <168799203+Mauriciom20@users.noreply.github.com>
2024-05-09 20:37:04 -0500

    Update lado-cubo-volumen.js

--- lado-cubo-volumen.js
+++ lado-cubo-volumen.js
@@ -1,5 +1,8 @@
 //1.Cree un programa que tome el lado de un cubo e imprima su volumen.
-
+let ladoCubo = parseFloat(prompt("Ingrese valor del lado del cubo"))
+    alert('los lados del cubo valen: ' + ladoCubo)
+    const volumen = ladoCubo**3
+    alert('el volumen del cubo es: ' + volumen)
 
 
 //Cree un programa que tome el precio de un producto e imprima su precio final al consumidor con un IVA de 19%.
