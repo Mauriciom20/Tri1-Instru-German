b08543a5fd8f54f000b6c0b927a06d3553d8e57f
Mauriciom20 <168799203+Mauriciom20@users.noreply.github.com>
2024-05-09 20:08:59 -0500

    Create lado-cubo-volumen.js

---
+++ lado-cubo-volumen.js
@@ -0,0 +1,6 @@
+//1.Cree un programa que tome el lado de un cubo e imprima su volumen.
+
+
+
+//Cree un programa que tome el precio de un producto e imprima su precio final al consumidor con un IVA de 19%.
+
