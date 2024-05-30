0587da6f09b22a3cb98cf30303204a838d1202cc
Mauriciom20 <168799203+Mauriciom20@users.noreply.github.com>
2024-05-29 21:06:37 -0500

    Update area.js

--- area.js
+++ area.js
@@ -1,31 +1,43 @@
-/*comentario de bloque
-Cree un programa que tome la base y la altura de un triángulo e imprima su área*/
+/*for (let index = 1; index <= 0; index++) {
+    console.log(index);
+};*/
 
-//comentario de una línea
 
-/*parseFloat toma un string y lo convierte a un numero flotante
-o sea decimal
-por qué la base es flotante? porque, la base de un triángulo 
-puede ser entera o decimal*/
-//ENTRADA DE DATOS
-let base = ""
-base =parseFloat(prompt("Ingrese la base"));
-    if (base < 0) {
-        alert("la base no puede ser negativa.");
-    } else {
-        alert("La base de tu triangulo es: " + base)
-    }
+// Ejercicio # 1
+//Cree un programa que imprima los números enteros entre 0 y 100 en orden ascendente y descendente(puede usar dos ciclos)
+/*let x = 0
+let y = 100
 
-let altura = ""
-altura = parseFloat(prompt("Ingrese la altura"));
-    if (altura < 0) {
-        alert("la altura no puede ser negativa.");
-    } else {
-        alert("La altura de tu triangulo es: " + altura)
-   
-    //PROCESO
-    const area = base * altura / 2;
-    //SALIDA
-    alert("El area del triangulo es: " + area + " U cuadradas");
-    }
+if (x <= y){
+    for (let index = x; index <= y; index++) {
+        console.log(index);
+    };
+} else {
+    console.log("Error, x no es menor o igual que y");
+}
 
+
+if (y >= x){
+    for (let index = y; index >= x; index++) {
+        console.log(index);
+    };
+} else {
+    console.log("Error, y no es mayor o igual que x");
+}*/
+
+// Ejercicio # 2
+//Cree un programa que imprima la tabla de multiplicar del 3, del 0 al 50.
+
+/*let x = 3
+let y = 1
+if (x >= y){
+    for (let index = x; index <= 50; y++) {
+        console.log(index);
+        index = x * y;
+    };
+} else {
+    console.log("Error, x no es menor o igual que y");
+}*/
+
+// Ejercicio # 3
+//Cree un programa que calcule el promedio de tres notas para n estudiantes.
