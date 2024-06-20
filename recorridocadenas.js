f6a28a6734fc1a41b1699606c5b37633e6f7e811
Mauriciom20 <168799203+Mauriciom20@users.noreply.github.com>
2024-06-19 20:53:24 -0500

    recorridocadenas.js

--- recorridocadenas.js
+++ recorridocadenas.js
@@ -1,3 +1,27 @@
+//las cadenas son inmutables
+let mensaje1 = "somos adso";
+//saber la longitud de la cadean(número de carateres)
+console.log("Longitud: ", mensaje1.length);
+let mensaje2 = "";
+console.log("Longitud: ", mensaje2.length);
+//lectura de caracteres. Se leen mediante su índice
+//cada caracter se encuentra indexado, siendo el índice del
+//primer caracter el 0, el índice del segundo carater el 1...
+//el último índice siempre es uno menos que la longitud
+console.log("Primer caracter: ", mensaje1[0]);
+console.log("último caracter: ", mensaje1[9]);
+console.log("caracter cinco: ", mensaje1[5]);
+console.log("caracter siete: ", mensaje1[7]);
+console.log("caracter no existente: ", mensaje1[15]);
+//NO SE PUEDEN MODIFICAR CADENAS, SON INMUTABLES
+//mensaje1[2] = "t";
+
+//RECORRIDO DE UNA CADENA
+let mensaje = "Somos programadores";
+for (let index = 0; index < mensaje.length; index++) {
+    console.log(mensaje[index]);//imprimirá cada caracter de la cadena
+}
+
 /*Ejercicio # 1
 Dada la cadena “Javascript es un buen lenguaje”:
 a) Leer e imprimir los elementos de índices 3, 6, 8
