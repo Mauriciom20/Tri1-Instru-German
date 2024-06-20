273422c27153705ea184481af41f9359093d7013
Mauriciom20 <168799203+Mauriciom20@users.noreply.github.com>
2024-06-19 20:56:00 -0500

    ciclosanidados.js

---
+++ ciclosanidados.js
@@ -0,0 +1,45 @@
+/*CICLOS - FINAL
+Parte 1:
+Usted es contratado por la empresa BioGenetics S.A. para crear un conjunto de programas de
+experimentación para su unidad de desarrollo e investigación llamada LabSADN (Laboratorio de
+Secuenciación del ADN). Como ellos saben que usted no es un experto conocedor del tema, ellos le dicen
+que no se preocupe, que para crear los programas solamente necesita saber lo siguiente:
+El ADN es la sigla de Ácido DesoxirriboNucleico, el cual es el material que almacena toda la
+información genética de un ser vivo.
+La información en el ADN se almacena como un código compuesto por cuatro bases químicas:
+Adenina (A), Timina (T), Citosina (C) y Guanina (G), para formar unidades llamadas pares de bases.
+https://medlineplus.gov/spanish/genetica/entender/basica/adn/
+Para resolver el literal b, y hacer una selección aleatoria de las dos bases solicitadas, básese en el
+siguiente artículo: https://www.w3schools.com/js/js_random.asp
+Cristiana, una joven investigadora del LabSADN, necesita un conjunto de programas que le ayuden a
+generar e imprimir cadenas de ADN de una longitud indicada por ella y además que dichas cadenas
+cumplan ciertas condiciones. Cada uno de los siguientes puntos debe ser un programa diferente.
+a. La cadena está formada por una única base: Adenina (A), Timina (T), Citosina (C) o Guanina (G),
+según se indique al momento de ejecutar el programa.
+b. La cadena tiene únicamente dos de las cuatro bases (seleccionadas aleatoriamente) y ubicadas en
+cualquier posición y orden dentro de la cadena.
+c. La cadena tiene una longitud múltiplo de 10 y la distribución de las bases debe respetar que: la
+cantidad de Citosina (C) es cuatro veces la cantidad de Guanina, Timina (T) es dos veces la cantidad de
+Guanina y Adenina (A) es tres veces la cantidad de Guanina. La cadena debe tener cuatro bases.
+El programa debe garantizar que el usuario ingrese una longitud múltiplo de 10. Si esta condición no se
+cumple, el programa debe informar y solicitar nuevamente los datos.
+Parte 2:
+Usted es contratado por la empresa SoftwareCorp S.A. para crear un conjunto de funcionalidades de
+apoyo para el desarrollo de productos.
+1. Cree un programa que muestre los números impares entre -100 y n.
+2. Cree un programa que recorra una cadena y muestre si esta contiene la letra z.
+3. Cree un programa que pida números al usuario
+indefinidamente hasta que este ingrese un número
+divisible entre 9.
+4. Construir un programa que reciba un número entero positivo no superior a 1.000.000.000 e indique si
+el número es primo. Por ejemplo: Dado el número 73 indicar true. Dado el número 35 indicar false
+5. Se trata de implementar un programa que devuelva la cantidad de ocurrencias del dígito que más se
+repite en un número entero no negativo. Por ejemplo, para el caso de 343331 el número que más se
+repite es 3, por ende se debe devolver 4, que es la cantidad de ocurrencias del 3.
+6. Cree un programa que dado un número, determine cuál es el dígito de mayor valor que lo compone.
+Por ejemplo, dado el número 67259711, el programa debe devolver 9, ya que es el dígito de mayor valor
+que compone el número.
+7. Cree un programa que valide el registro de un usuario, de tal manera que pida a éste su email y su
+contraseña, si el email contiene el @ y la contraseña tiene una longitud entre 8 y 15, el programa deberá
+mostrar “Registro exitoso”, de lo contrario, el programa deberá seguir pidiendo los datos al usuario hasta
+que se ingresen datos válidos.*/
